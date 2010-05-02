37fdf32cbc0c4ed9fdf42619f9713bf8e11e67ae
ben@benatkin.com <ben@benatkin.com>
2010-05-02 02:34:28 -0600

    created web prototype

---
+++ web/teatimer.js
@@ -0,0 +1,26 @@
+var endDate;
+var interval;
+
+function updateTime() {
+  var currentDate = new Date();
+  var difference = (endDate - currentDate) / 1000;
+  var minutes = Math.floor(Math.abs(difference / 60));
+  var seconds = Math.floor(Math.abs(difference % 60));
+  $('.time').text(minutes + ':' + (seconds > 9 ? '' : '0') + seconds);
+  if (Math.floor(difference) <= 0) {
+    if (interval != undefined) clearInterval(interval);
+    alert('your tea is ready!');
+  }
+}
+
+$(document).ready(function() {
+  $('.startTimer').click(function() {
+    var minutes = $(this).hasClass('green') ? 2 : 5;
+    endDate = new Date();
+    endDate.setTime(endDate.getTime() + minutes * 60 * 1000 + 900); 
+    if (interval != undefined) clearInterval(interval);
+
+    updateTime();
+    interval = setInterval(updateTime, 100);
+  });
+});
